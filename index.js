1d069494fd7b24be19d31953244e4f0338922152
Milechwan <mscc@cin.ufpe.br>
2018-06-23 12:04:00 -0300

    Página de recomendação em andamento

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-d3.csv("./data/dataset_teste.csv", (data => {
+d3.csv("./data/dataset.csv", (data => {
  
     var dados = data.map(e => {
         return {
-            uIdCheckin: e["User ID_checkin"],
-            venueId: e["Venue ID"],
-            tags: e["Tags"],
-            uIdCommenter: e["User ID_commenter"],
-            comment: e["Comment"],
+            uIdCheckin: e["User_Id"],
+            venueId: e["Venues_Id"],
+            tagBar: e["Bar"],
+            tagBurguer: e["Burguer"]
         }
     });
-    cleanData(dados)
-    //console.log(dados)
+    //cleanData(dados)
+    console.log(dados);
 }));
  
 var a = [{}];
 var invalid = 'none None "" unknown';
- 
-var keys = ["uIdCheckin", "venueId", "tags", "uIdCommenter", "comment"];
+var lin;
+var resp = [{}]; 
+var keys = ["uIdCheckin", "venueId", "tagBar","tagBurguer"];
 var cont = 0;
 function cleanData(data){
     data.forEach(e => {
@@ -26,10 +26,17 @@ function cleanData(data){
                 aux++;
                 if(aux>0) break;
             }
-
-            if((keys[i] == "tags") && ((e[keys[i]].includes("bar,")) ||(e[keys[i]].includes("bar\\")) )){
-                cont++;
+            if((keys[i] == "tagBar") /*&& ((e[keys[i]].includes("bar,")) ||(e[keys[i]].includes("bar\\")) )*/){
+                //cont++;
+                lin = e[keys[i]].split(",");
+                    for(var j = 0; j < lin.length; j++){
+                        if(!resp.includes(lin[i]) && (lin[i] != undefined) && (lin[i] != ""))
+                        resp.push(lin[i]);
+                    }
             }
+            /*if((keys[i] == "tags") && ((e[keys[i]].includes("bar,")) ||(e[keys[i]].includes("bar\\")) )){
+                cont++;
+            }*/
         }
         if(aux<=0)a.push(e);
     });
